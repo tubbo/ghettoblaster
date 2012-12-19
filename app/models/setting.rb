7bb3d143eeda95330d4b0a0e3d6f8fc63d0f5433
Tom Scott <tubbo@psychedeli.ca>
2012-12-19 00:56:50 -0500

    Add fixtures for default settings

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,4 +1,14 @@
 class Setting < ActiveRecord::Base
   validates_presence_of :key
   attr_accessible :key, :value
+
+  def self.for a_key
+    setting = Setting.where(key: "#{a_key}").first
+
+    if setting.present?
+      setting.value
+    else
+      raise "Error: Setting not found."
+    end
+  end
 end
