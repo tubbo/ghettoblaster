4f1610da2e432a23777b26a7e5dfa1b2fce2e1d9
Tom Scott <tubbo@psychedeli.ca>
2012-12-19 01:30:06 -0500

    Ghettoblaster is a better name

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-    # Wunderkind
+# Ghettoblaster
 
-    A thorough description of the application.
+Ghettoblaster is your personal email announcement list. Write your emails in Markdown, preview
+them with Mailcatcher, and obtain new email submissions from an innocuous looking front page. It
+is in use to manage the Wonder Bars' email list.
--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.register_page "Dashboard" do
   menu :priority => 1, :label => "Overview"
 
-  content title: "Settings" do
-    para "Welcome to Wunderkind. Have a look around!"
+  content title: "Welcome to your Ghettoblaster" do
+    para "This is your admin panel. From here, you can see your entire email subscription list (and make changes) as well as compose new messages."
   end
 end
