9237199b6f17dc9fd7266e424f22434ca845d0cc
Tom Scott <tubbo@psychedeli.ca>
2012-12-19 02:00:46 -0500

    Not needed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-unless User.where(email: "tom@thewonderbars.com").any?
-  User.create! \
-    email: 'tom@thewonderbars.com',
-    password: 'password',
-    password_confirmation: 'password'
-end
-
 unless Setting.all.any?
   yaml = YAML::load_file("#{Rails.root}/config/settings.yml")
   yaml.keys.each { |key,val| Setting.create(key: key, value: val) }
