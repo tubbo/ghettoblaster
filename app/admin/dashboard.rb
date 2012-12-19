27e19332ee24c0bbd96125542f589bd408c01535
Tom Scott <tubbo@psychedeli.ca>
2012-12-19 01:41:39 -0500

    Fix admin dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,7 +1,9 @@
 ActiveAdmin.register_page "Dashboard" do
   menu :priority => 1, :label => "Overview"
 
-  content title: "Welcome to your Ghettoblaster" do
-    para "This is your admin panel. From here, you can see your entire email subscription list (and make changes) as well as compose new messages."
+  content title: "Welcome" do
+    h2 "Welcome to your Ghettoblaster"
+    h3 "This is your admin panel."
+    h3 "From here, you can see your entire email subscription list (and make changes) as well as compose new messages."
   end
 end
