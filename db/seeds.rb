54abc5c1c01f82d7ec57d7a1538352ed3171c5df
Tom Scott <tubbo@psychedeli.ca>
2013-01-18 14:33:03 -0500

    Fix seed file, hopefully this will fix the build issue.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,10 @@
 unless Setting.all.any?
   yaml = YAML::load_file("#{Rails.root}/config/settings.yml")
-  yaml.keys.each { |key,val| Setting.create(key: key, value: val) }
+  yaml.keys.each do |key,val|
+    if Setting.for(:"#{key}").blank?
+      Setting.create(key: key, value: val)
+    else
+      Setting.where(key: key).first.update_attributes(value: val)
+    end
+  end
 end
