8af0e0ae92eb47c54a1e8b5b0a1c74f9e39ba521
Tom Scott <tubbo@psychedeli.ca>
2013-01-18 17:33:11 -0500

    Run an airbrake:deploy after deploying to Heroku
    Notifies Airbrake that a new build is on the production server,
    and resolves all open issues.

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -3,7 +3,6 @@ module MarkdownHelper
     markdown_parser.render(source).html_safe
   end
 
-private
   def markdown_parser
     @parser ||= Redcarpet::Markdown.new Redcarpet::Render::HTML, \
       autolink: true, space_after_headers: true
