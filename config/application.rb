2a525541a50c9a3ded00cd7eac710be3b1864be9
Tom Scott <tubbo@psychedeli.ca>
2013-10-02 18:45:36 -0400

    Use `rake test` for running tests on CI

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,19 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+require "rails"
+
+%w(
+  active_record
+  action_controller
+  action_view
+  action_mailer
+  sprockets
+).each do |framework|
+  begin
+    require "#{framework}/railtie"
+  rescue LoadError
+  end
+end
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
