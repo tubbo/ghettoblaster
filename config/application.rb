e9b3d4aec051161bcd86be43eba4d0542caaeda9
Tom Scott <tubbo@psychedeli.ca>
2013-11-17 22:41:22 -0500

    Clean up Bundler.require

--- config/application.rb
+++ config/application.rb
@@ -11,16 +11,11 @@ require "rails"
 ).each do |framework|
   begin
     require "#{framework}/railtie"
-  rescue LoadError
-  end
+  rescue LoadError; end
 end
 
-if defined?(Bundler)
-  # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
-  # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
-end
+Bundler.require \
+  :framework, :engines, :libraries, *Rails.groups(:assets => %w(development test))
 
 module Ghettoblaster
   class Application < Rails::Application
