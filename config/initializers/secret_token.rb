5e70080086801675d52ec224868e052a71325b11
Tom Scott <tscott@telvue.com>
2013-11-28 03:06:33 -0500

    Make rails secret...secret. Mostly.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Ghettoblaster::Application.config.secret_token = 'b08fd8d243631b120692de4d2710b6d9750578d3744f367484a04112a35e50f72ac359daf1f16a9ccdab7957041b428982dfb0befa81859b883f450e97ff569f'
+Ghettoblaster::Application.config.secret_token = ENV['RAILS_SECRET']
