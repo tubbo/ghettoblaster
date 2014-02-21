317d7de5e4eb3fa1d41e6acd83ee15db66356414
Tom Scott <tscott@telvue.com>
2014-02-21 14:16:25 -0500

    Add sendgrid config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,4 +67,15 @@ Ghettoblaster::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # Use Sendgrid for sending emails
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 end
